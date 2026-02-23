c9004a16ae41aaa845502c6733d27b92676b4c59
TysonTien0111 <TysonNguyen0111@gmail.com>
2026-02-23 09:32:38 -0800

    Implemented New Test Cases

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -5,6 +5,9 @@ def testBasicSequence():
     expected = "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
     assert foo_bar_baz(15) == expected
 
+    expected = "1 2"
+    assert foo_bar_baz(2) == expected
+
 def testSingleValues():
     assert foo_bar_baz(1) == "1"
     assert foo_bar_baz(3) == "1 2 Foo"
